674b138659252a4c49315259b7db2d4fb30a6f4d
wvahidy <93625194+wvahidy@users.noreply.github.com>
2024-08-18 14:05:13 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Global } from '@emotion/react';
+import GlobalState from './context';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <GlobalState>
+<ChakraProvider theme ={theme}>
     <App />
-  </React.StrictMode>
+    </ChakraProvider>
+    </GlobalState>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
